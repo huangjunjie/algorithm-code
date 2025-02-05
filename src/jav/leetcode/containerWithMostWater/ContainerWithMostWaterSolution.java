ca04497691409d7fb43719de97a5df86aac12df6
HuangJunJie.Stone <1157510753@qq.com>
2025-02-05 09:01:45 +0800

    huangjunjie[2025/2/5 08:59] resolve container with most water

---
+++ src/jav/leetcode/containerWithMostWater/ContainerWithMostWaterSolution.java
@@ -0,0 +1,38 @@
+package jav.leetcode.containerWithMostWater;
+
+public class ContainerWithMostWaterSolution {
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(new ContainerWithMostWaterSolution().maxArea(height));
+    }
+
+
+    /**
+     * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+     * <p>
+     * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+     * <p>
+     * Return the maximum amount of water a container can store.
+     * <p>
+     * Notice that you may not slant the container.
+     * <p>
+     * dp(x,y)  =  max((y - x) * min(height[x],height[y]))
+     * <p>
+     * 比较面积级别
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int result = -1;
+        for (int i = 0; i < height.length - 1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                if (height[i] * height.length < result) break;
+                int area = (j - i) * Math.min(height[i], height[j]);
+                if (result < area) result = area;
+            }
+        }
+        return result;
+    }
+}
